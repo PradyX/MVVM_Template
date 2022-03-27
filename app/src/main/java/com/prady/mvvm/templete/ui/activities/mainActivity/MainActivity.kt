403e51fe744b6d50cf958503e09cfa804005a0a8
pradyX <pradyumnbhushan@gmail.com>
2022-03-27 22:31:44 +0530

    Setup live internet connectivity check
    Signed-off-by: pradyX <pradyumnbhushan@gmail.com>

--- app/src/main/java/com/prady/mvvm/templete/ui/activities/mainActivity/MainActivity.kt
+++ app/src/main/java/com/prady/mvvm/templete/ui/activities/mainActivity/MainActivity.kt
@@ -6,12 +6,14 @@ import android.util.Log
 import com.prady.mvvm.templete.R
 import com.prady.mvvm.templete.databinding.ActivityMainBinding
 import com.prady.mvvm.templete.ui.activities.mainActivity.fragments.HomeFragment
+import com.prady.mvvm.templete.utils.ConnectionLiveData
 import com.prady.srmgpc_user.helpers.PreferenceManager
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var preferenceManager : PreferenceManager
     private lateinit var binding : ActivityMainBinding
+    private lateinit var connectionLiveData: ConnectionLiveData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +26,21 @@ class MainActivity : AppCompatActivity() {
 
         if(savedInstanceState == null){
             supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+        }
+
+        connectionLiveData = ConnectionLiveData(this)
+        connectionLiveData.observe(this) { isNetworkAvailable ->
+            isNetworkAvailable?.let {
+                updateUI(it)
+            }
+        }
+    }
+
+    private fun updateUI(it: Boolean) {
+        if(it){
+            Log.e("Network", "Available")
+        }else{
+            Log.e("Network", "Not Available")
         }
     }
 }
