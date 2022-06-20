a74f5178f47479901f00604f637940fc3d0bb837
pradyX <pradyumnbhushan@gmail.com>
2022-06-20 21:42:47 +0530

    helpers: shared pref to singleton
    Signed-off-by: pradyX <pradyumnbhushan@gmail.com>

--- app/src/main/java/com/prady/mvvm/template/helpers/PreferenceManager.kt
+++ app/src/main/java/com/prady/mvvm/template/helpers/PreferenceManager.kt
@@ -1,52 +1,44 @@
 /*
  * *
- *  * Created by prady on 3/29/22, 1:32 PM
+ *  * Created by prady on 6/8/22, 11:14 PM
  *  * Copyright (c) 2022 . All rights reserved.
- *  * Last modified 3/29/22, 12:37 PM
+ *  * Last modified 6/8/22, 11:14 PM
  *
  */
+package com.prady.mvvm.template.helpers
 
-package com.prady.srmgpc_user.helpers
-
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
 
-class PreferenceManager(var context: Context) {
-    var sharedPreferences: SharedPreferences =
-        context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
-    var editor: SharedPreferences.Editor = sharedPreferences.edit()
+class PreferenceManager private constructor(context: Context) {
+
+    var sharedPreferences: SharedPreferences
+    var context: Context
+    var editor: SharedPreferences.Editor
 
     companion object {
-        private const val PREF_NAME = "data"
+        @SuppressLint("StaticFieldLeak")
+        lateinit var instance: PreferenceManager
+            private set
+
+        @JvmStatic
+        fun init(context: Context) {
+            instance = PreferenceManager(context)
+        }
     }
 
-    var userName: String?
-        get() = sharedPreferences.getString("username", "")
-        set(userName) {
-            editor.putString("username", userName)
-            editor.commit()
-        }
+    init {
+        sharedPreferences = context.getSharedPreferences("pref", Context.MODE_PRIVATE)
+        this.context = context
+        editor = sharedPreferences.edit()
+    }
 
-    var email: String?
-        get() = sharedPreferences.getString("email", "")
-        set(email) {
-            editor.putString("email", email)
-            editor.commit()
-        }
-
-    var profilePic: String?
-        get() = sharedPreferences.getString("profilePic", "")
-        set(pic) {
-            editor.putString("profilePic", pic)
-            editor.commit()
-        }
-
-    var mobile: String?
-        get() = sharedPreferences.getString("mobile", "")
-        set(mobile) {
-            editor.putString("mobile", mobile)
-            editor.commit()
-        }
+    fun clear() {
+        editor.clear()
+        editor.apply()
+        editor.commit()
+    }
 
     var userid: String?
         get() = sharedPreferences.getString("userid", "")
@@ -54,17 +46,28 @@ class PreferenceManager(var context: Context) {
             editor.putString("userid", userid)
             editor.commit()
         }
-
-    var city: String?
-        get() = sharedPreferences.getString("city", "")
-        set(city) {
-            editor.putString("city", city)
+    var email: String?
+        get() = sharedPreferences.getString("email", "")
+        set(email) {
+            editor.putString("email", email)
             editor.commit()
         }
-
-    fun clear() {
-        editor.clear()
-        editor.apply()
-        editor.commit()
-    }
+    var token: String?
+        get() = sharedPreferences.getString("Token", "")
+        set(Token) {
+            editor.putString("Token", Token)
+            editor.commit()
+        }
+    var tempToken: String?
+        get() = sharedPreferences.getString("TempToken", "")
+        set(Token) {
+            editor.putString("TempToken", Token)
+            editor.commit()
+        }
+    var loggedIn: String?
+        get() = sharedPreferences.getString("LoggedIn", "")
+        set(Token) {
+            editor.putString("LoggedIn", Token)
+            editor.commit()
+        }
 }
