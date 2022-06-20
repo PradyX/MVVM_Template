2ff7318ea81fe833ce0f74d376fe11db61d955cf
pradyX <pradyumnbhushan@gmail.com>
2022-06-20 22:54:30 +0530

    utils : toast & snackbar extensions
    Signed-off-by: pradyX <pradyumnbhushan@gmail.com>

--- app/src/main/java/com/prady/mvvm/template/utils/Utils.kt
+++ app/src/main/java/com/prady/mvvm/template/utils/Utils.kt
@@ -8,6 +8,11 @@
 
 package com.prady.mvvm.template.utils
 
+import android.content.Context
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
 class Utils {
 
     // Checking nullability
@@ -19,5 +24,39 @@ class Utils {
     }
 */
 
+    //Extension function
+    fun String?.valid(): Boolean =
+        this != null && !this.equals("null", true)
+                && this.trim().isNotEmpty()
+/* Usage at call site
+   if(data.valid())
+ */
 
+    // Toast extensions
+    fun Context.showShotToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun Context.showLongToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    // Snack bar Extensions
+    fun View.showShotSnackbar(message: String) {
+        Snackbar.make(this, message, Snackbar.LENGTH_SHORT).show()
+    }
+
+    fun View.showLongSnackbar(message: String) {
+        Snackbar.make(this, message, Snackbar.LENGTH_LONG).show()
+    }
+
+    fun View.snackBarWithAction(
+        message: String, actionlable: String,
+        block: () -> Unit
+    ) {
+        Snackbar.make(this, message, Snackbar.LENGTH_LONG)
+            .setAction(actionlable) {
+                block()
+            }
+    }
 }
